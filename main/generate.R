bc2da8aeab9e202336e256ccf18875d160ed1d5d
Will Zywiec <30445407+wzywiec@users.noreply.github.com>
2019-03-30 11:47:49 -0700

    Update generate.R

--- main/generate.R
+++ main/generate.R
@@ -8,14 +8,14 @@
 Generate <- function(deck.size) {
 
   # generate random uniform data
-  mass <- sample(seq(10, 2000, 10), deck.size, replace = TRUE)
+  mass <- sample(seq(10, 1000, 10), deck.size, replace = TRUE)
   form <- sample(c('alpha', 'oxide'), deck.size, replace = TRUE)
   mod <- sample(c('ch2', 'sepiolite', 'h2o', 'none'), deck.size, replace = TRUE)
   rad <- sample(seq(0.5, 18, 0.5) * 2.54, deck.size, replace = TRUE) # 18 in = 45.72 cm
   ref <- sample(c('al', 'be', 'beo', 'cs', 'cu', 'c', 'ch2', 'ss304', 'ta', 'v', 'h2o', 'none'), deck.size, replace = TRUE)
   dim <- sample(seq(0.5, 18, 0.5) * 2.54, deck.size, replace = TRUE) # 18 in = 45.72 cm
   shape <- sample(c('sph', 'rcc', 'rpp'), deck.size, replace = TRUE)
-  ht <- sample(seq(1, 36, 1) * 2.54, deck.size, replace = TRUE) # 36 in = 91.44 cm
+  ht <- sample(seq(0.5, 36, 0.5) * 2.54, deck.size, replace = TRUE) # 36 in = 91.44 cm
 
   # build input decks and run MCNP
   for (i in 1:deck.size) {
